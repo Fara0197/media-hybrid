f0843f986e0e46ed8baddb68e7653b40387d982c
Nasra <fara0197@algonquinlive.com>
2022-01-30 14:27:05 -0500

    updated index.html: audio class

--- js/controls.js
+++ js/controls.js
@@ -1,1 +1,30 @@
-'use strict'
+"use strict";
+
+/**
+ *Hybrid 2: Tasks
+ * 1. listen for duration change event..
+ * 2. Listen for click events to play, pause and stop the first track from the array
+ * 3. Display thumbnail image in the player for the song currently playing
+ * 4. Display one of the play and pause button..if playing then play button should be hidden.
+ * 5. Add a CSS active class to song playing & media player area.
+ */
+
+
+//get elements 
+  let player = document.getElementById("player");
+  let audio = document.getElementById("audio");
+  let btnPlay = document.getElementById("buttonPlay");
+  let btnStop = document.getElementById("buttonStop");
+  let btnPause = document.getElementById("buttonPause");
+
+//add event listeners 
+
+
+
+//functions for event listeners 
+
+
+
+
+
+
